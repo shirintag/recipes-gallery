85199bd14cf84a0c0a8373ed02904588271919ba
Shirin Taghavi <taghavi.shirin@gmail.com>
2020-06-12 11:06:20 +0200

    Make the grid responsive, some small css chanes

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -9,14 +9,22 @@ const RecipeCard = (props) => {
       <div className="recipe-heart">
         <Heart />
       </div>
+      <div className="recipe-image-container">
+        <img
+          className="recipe-image"
+          src={
+            "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg"
+          }
+          alt={props.name}
+        />
+      </div>
 
-      <img className="recipe-image" src={props.image} />
       <div className="recipe-data f-subcopy">
         <StarRating totalStars={5} />
         <div className="f-title">{props.name}</div>
         <div className="f-subtitle">{props.headline}</div>
-        <span>calories: {props.calories}</span>
-        <span>time: {props.time}</span>
+        <span>calories: {props.calories || "--"}</span>
+        <span>Time: {props.time.substring(2) || "--"}</span>
       </div>
     </div>
   );
