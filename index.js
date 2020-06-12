4e91b2688c9f9990810bb0a648f4fee1d63328ca
Shirin Taghavi <taghavi.shirin@gmail.com>
2020-06-12 15:56:55 +0200

    Add backend server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static("dist"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./dist/index.html"));
+});
+
+app.get("/recipes", (req, res) => {
+  res.sendFile(path.resolve("./recipes.json"));
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on 8080!");
+});
